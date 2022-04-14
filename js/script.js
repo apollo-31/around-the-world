2873f7e1752123280f1b9c45cdb6fb9270fb68d5
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-14 01:15:19 -0500

    change script.js structure

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
 var primarySlide = document.querySelector(".primarySlide");
 var secondarySlide = document.querySelector(".trip-summary");
-var destinationInput = document.querySelector("#destination");
+var destinationInput = document.querySelector("#destination").value;
 var goingDate = document.querySelector("#goingDate");
 var comingBackDate = document.querySelector("#comingBackDate");
+
+var leavingDatePicker = new Datepicker(goingDate, {
+        autohide: "true"
+    });
+    
+
+var comingBackDatePicker = new Datepicker(comingBackDate, {
+        autohide: "true"
+    });
+    
+var leavingDate = leavingDatePicker.getDate("yyyy-mm-dd");
+var comingDate = comingBackDatePicker.getDate("yyyy-mm-dd");
 
 var firstSlide = function() {
     primarySlide.style.display = "initial";
     secondarySlide.style.display = "none";
 
-    var leavingDatePicker = new Datepicker(goingDate, {
-        autohide: "true"
-    });
     
-
-    var comingBackDatePicker = new Datepicker(comingBackDate, {
-        autohide: "true"
-    });
-    
-    var leavingDate = leavingDatePicker.getDate("yyyy-mm-dd");
-    var comingDate = comingBackDatePicker.getDate("yyyy-mm-dd");
-
-    var destName = destinationInput.value;
 
     var goBtn = document.querySelector("#goBtn");
     goBtn.addEventListener("click", secondSlide);
 
+    console.log(destinationInput);
+
     
 }
 
-var secondSlide = function(destName,leavingDate,comingDate) {
+var secondSlide = function() {
     secondarySlide.style.display = "initial";
     primarySlide.style.display = "none";
     
-        var getCoordsApi = "https://api.openweathermap.org/data/2.5/weather?q=" + destName + "&units=imperial&appid=5a5307ea2f6a35b62ce0461de8e45a8d";
+        var getCoordsApi = "https://api.openweathermap.org/data/2.5/weather?q=" + destinationInput + "&units=imperial&appid=5a5307ea2f6a35b62ce0461de8e45a8d";
 
         fetch(getCoordsApi)
         .then(function(response) {
@@ -43,12 +45,10 @@ var secondSlide = function(destName,leavingDate,comingDate) {
 
             var lon = data.coord.lon;
             var lat = data.coord.lat;
-            var nameOfCity = data.name;
 
-            return[lat,lon,nameOfCity];
+            return[lat,lon];
 
-        // }).then(function([lat,lon,nameOfCity]) {
-        //     var weatherApi = ""
+        // }).then(function([lat,lon])
         })
     
 }
