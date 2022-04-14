ec22d9af720f3ca12b673e8c87b59b1e031de067
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-14 00:44:43 -0500

    begin weather fetch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var primarySlide = document.querySelector(".primarySlide");
 var secondarySlide = document.querySelector(".trip-summary");
-var destinationInput = document.querySelector("#destination").value;
+var destinationInput = document.querySelector("#destination");
 var goingDate = document.querySelector("#goingDate");
 var comingBackDate = document.querySelector("#comingBackDate");
 
@@ -20,19 +20,37 @@ var firstSlide = function() {
     var leavingDate = leavingDatePicker.getDate("yyyy-mm-dd");
     var comingDate = comingBackDatePicker.getDate("yyyy-mm-dd");
 
+    var destName = destinationInput.value;
+
     var goBtn = document.querySelector("#goBtn");
     goBtn.addEventListener("click", secondSlide);
+
+    
 }
 
-var secondSlide = function() {
+var secondSlide = function(destName,leavingDate,comingDate) {
     secondarySlide.style.display = "initial";
     primarySlide.style.display = "none";
     
-    
-    
+        var getCoordsApi = "https://api.openweathermap.org/data/2.5/weather?q=" + destName + "&units=imperial&appid=5a5307ea2f6a35b62ce0461de8e45a8d";
 
-    var nextpageBtn = document.querySelector("#selectFlightBtn");
-    nextpageBtn.addEventListener("click", thirdSlide);
+        fetch(getCoordsApi)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+            var nameOfCity = data.name;
+
+            return[lat,lon,nameOfCity];
+
+        // }).then(function([lat,lon,nameOfCity]) {
+        //     var weatherApi = ""
+        })
+    
 }
 
 
