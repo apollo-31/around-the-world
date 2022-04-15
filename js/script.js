f811e2c9668a7592cc49efec95d61395e5444cfa
MiaLaFleur <mia.lafleur@yahoo.com>
2022-04-14 20:21:16 -0500

    some extra touches

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var primarySlide = document.querySelector(".primarySlide");
 var secondarySlide = document.querySelector(".trip-summary");
 var eventSection = document.querySelector(".local");
+var forecastSection = document.querySelector("#forecastSection");
 
 var goingDate = document.querySelector("#goingDate");
 var comingBackDate = document.querySelector("#comingBackDate");
@@ -13,8 +14,9 @@ var leavingDatePicker = new Datepicker(goingDate, {
 var comingBackDatePicker = new Datepicker(comingBackDate, {
         autohide: "true"
     });
-    
 
+var leavingDate = leavingDatePicker.getDate("yyyy.mm.dd");
+var comingDate = comingBackDatePicker.getDate("yyyy.mm.dd");
 
 
 var firstSlide = function() {
@@ -34,38 +36,42 @@ var firstSlide = function() {
 
         secondSlide();
     });
-
-
-    
+ 
 }
 
-var lon;
-var lat;
+
 
 var secondSlide = function() {
-    var leavingDate = leavingDatePicker.getDate("yyyy.mm.dd");
-    var comingDate = comingBackDatePicker.getDate("yyyy.mm.dd");
-    console.log(leavingDate);
     secondarySlide.style.display = "initial";
     primarySlide.style.display = "none";
 
     var destName = localStorage.getItem("destination");
         var getCoordsApi = "https://api.openweathermap.org/data/2.5/weather?q=" + destName + "&units=imperial&appid=5a5307ea2f6a35b62ce0461de8e45a8d";
 
-        fetch(getCoordsApi) 
+        fetch(getCoordsApi)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             console.log(data);
 
-            lon = data.coord.lon;
-            lat = data.coord.lat;
+            var todayWeather = document.createElement("div");
+            todayWeather.className = "todayWeather";
+            var todayDescription = document.createElement("p");
+            todayDescription.className = "todayDescription";
+            todayDescription.textContent = data.weather[0].description;
 
+            todayWeather.appendChild(todayDescription);
+            forecastSection.appendChild(todayWeather);
+
+           var lon = data.coord.lon;
+           var lat = data.coord.lat;
+
+           return [lat,lon];
         })
-        .then(function() {
+        .then(function([lat,lon]) {
             var eventsApi = "https://app.ticketmaster.com/discovery/v2/events.json?latlong=" + lat + "," + lon + "&apikey=FKA8aYhM8iHaCS67OhDL1AgP1DUITuPw&size=10";
-        
+
         fetch(eventsApi)
         .then(function(response) {
             return response.json();
